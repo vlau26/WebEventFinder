22fecf977e0ecefc9f2708b847f2c4b2676bbe28
Vivian Lau <lauv26@gmail.com>
2019-07-12 01:20:00 -0700

    Adding catalog and pic controller classes + paginated view model, + event pictures in wwwroot folder. Connection string added to app settings JSON file. Added cache file type to gitignore

---
+++ EventBriteAssignment3A/Controllers/CatalogController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventBriteAssignment3A.ViewModels;
+using EventBriteCatalog.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EventBriteAssignment3A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _catalogContext;
+        private readonly IConfiguration _configuration;
+
+        public CatalogController(CatalogContext catalogContext, IConfiguration configuration)
+        {
+            _catalogContext = catalogContext;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogCategories()
+        {
+            var events = await _catalogContext.CatalogCategories.ToListAsync();
+            return Ok(events);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogLocations()
+        {
+            var events = await _catalogContext.CatalogLocations.ToListAsync();
+            return Ok(events);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Events([FromQuery]int pageSize = 6, [FromQuery]int pageIndex = 0)
+        {
+            var eventsCount = await _catalogContext.CatalogItems.LongCountAsync();
+
+            var events = await _catalogContext.CatalogItems
+                 .OrderBy(c => c.EventName)
+                 .Skip(pageSize * pageIndex)
+                 .Take(pageSize)
+                 .ToListAsync();
+            events = ChangePictureUrl(events);
+            var model = new PaginatedItemsViewModel<CatalogItem>
+            {
+                PageSize = pageSize,
+                PageIndex = pageIndex,
+                Count = eventsCount,
+                Data = events
+            };
+            return Ok(model);
+        }
+
+        private List<CatalogItem> ChangePictureUrl(List<CatalogItem> events)
+        {
+            events.ForEach(c => c.PictureUrl = c.PictureUrl.Replace("http://externalcatalogbaseurltobereplaced", _configuration["ExternalCatalogBaseUrl"]));
+            return events;
+        }
+
+/*        [HttpGet]
+        [Route("[action]/EventName/{EventName:minlength(1)}")]
+        public async Task<IActionResult> Events(
+             string? EventName, int? EventId,
+             [FromQuery]int pageSize = 6,
+             [FromQuery]int pageIndex = 0)
+        {
+            var root = (IQueryable<CatalogItem>)_catalogContext.CatalogItems;
+
+            if (EventId.HasValue)
+            {
+                root = root.Where(c => c.EventId == EventId);
+            }
+
+            if (EventName.HasValue)
+            {
+                root = root.Where(c => c.EventName == EventName);
+            }
+
+            var totalEvents = await root.LongCountAsync();
+
+            var eventsOnPage = await root
+                               .OrderBy(c => c.EventName)
+                               .Skip(pageSize * pageIndex)
+                               .Take(pageSize)
+                               .ToListAsync();
+            eventsOnPage = ChangePictureUrl(eventsOnPage);
+            var model = new PaginatedItemsViewModel<CatalogItem>
+            {
+                PageSize = pageSize,
+                PageIndex = pageIndex,
+                Count = eventsCount,
+                Data = events
+            };
+            return Ok(model);
+        }
+
+
+        [HttpGet]
+        [Route("events/{id:int}")]
+
+        public async Task<IActionResult> GetEventsById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Incorrect Id!");
+            }
+
+            var event = await _catalogContext.CatalogItem.SingleOrDefaultAsync(c => c.Id == id);
+            if (event == null)
+            {
+            return NotFound("not found");
+        }
+
+            event.PictureUrl = event.PictureUrl
+                    .Replace("http://externalcatalogbaseurltobereplaced",
+                    _configuration["ExternalCatalogBaseurl"]);
+            return Ok(event);
+        }*/
+    }
+}
