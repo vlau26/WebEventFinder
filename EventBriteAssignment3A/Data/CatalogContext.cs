be2884331a67c75db3e108f091fba6afa183dcbc
dnsanche <36754228+dnsanche@users.noreply.github.com>
2019-07-11 23:25:02 -0700

    Add constructor

--- EventBriteAssignment3A/Data/CatalogContext.cs
+++ EventBriteAssignment3A/Data/CatalogContext.cs
@@ -1,7 +1,6 @@
 ﻿using EventBriteCatalog.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,15 +9,58 @@ namespace EventBriteAssignment3A.Data
 {
     public class CatalogContext : DbContext
     {
+        public CatalogContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        //Creation of tables
         public DbSet<CatalogCategory> CatalogCategories { get; set; }
         public DbSet<CatalogItem> CatalogItems { get; set; }
         public DbSet<CatalogLocation> CatalogLocations { get; set; }
 
+        //Specify all models that will be build
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CatalogCategory>(ConfigureCatalogCategory);
             modelBuilder.Entity<CatalogItem>(ConfigureCatalogItem);
             modelBuilder.Entity<CatalogLocation>(ConfigureCatalogLocation);
+        }
+
+        //Specifies how to build each model: Location, Item and brand tables. 
+        //Specifies what properties are required.
+                private void ConfigureCatalogItem(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog");
+            builder.Property(c => c.EventId)
+                .IsRequired()
+                .ForSqlServerUseSequenceHiLo("catalog_hilo");
+
+            builder.Property(c => c.EventName).IsRequired().HasMaxLength(50);
+
+            builder.Property(c => c.Description).IsRequired().HasMaxLength(300);
+
+            builder.Property(c => c.Price).IsRequired();
+
+            builder.HasOne(c => c.Category).WithMany().HasForeignKey(c => c.CatalogCategoryId);
+
+            builder.HasOne(c => c.Location).WithMany().HasForeignKey(c => c.CatalogLocationId);
+
+            builder.Property(c => c.EventStartTime).IsRequired();
+
+            builder.Property(c => c.EventEndTime).IsRequired();
+
+        }
+
+        private void ConfigureCatalogCategory(
+            EntityTypeBuilder<CatalogCategory> builder)
+        {
+            builder.ToTable("CatalogCategories");
+            builder.Property(c => c.Id)
+                .IsRequired()
+                .ForSqlServerUseSequenceHiLo("catalog_category_hilo");
+
+            builder.Property(c => c.Category).HasMaxLength(100);
         }
 
         private void ConfigureCatalogLocation(EntityTypeBuilder<CatalogLocation> builder)
@@ -32,35 +74,5 @@ namespace EventBriteAssignment3A.Data
                 .HasMaxLength(100);
         }
 
-        private void ConfigureCatalogItem(
-            EntityTypeBuilder<CatalogItem> builder)
-        {
-            builder.ToTable("Catalog");
-            builder.Property(c => c.EventId)
-                .IsRequired()
-                .ForSqlServerUseSequenceHiLo("catalog_hilo");
-            
-            builder.Property(c => c.EventName).IsRequired().HasMaxLength(50);
-
-            builder.Property(c => c.Description).IsRequired().HasMaxLength(300); 
-
-            builder.Property(c => c.Price).IsRequired();
-
-            builder.HasOne(c => c.Category).WithMany().HasForeignKey(c => c.CatalogCategoryId);
-
-            builder.HasOne(c => c.Location).WithMany().HasForeignKey(c => c.CatalogLocationId);    
-            
-        }
-
-        private void ConfigureCatalogCategory(
-            EntityTypeBuilder<CatalogCategory> builder)
-        {
-            builder.ToTable("CatalogCategories");
-            builder.Property(c => c.Id)
-                .IsRequired()
-                .ForSqlServerUseSequenceHiLo("catalog_category_hilo");
-        
-            builder.Property(c => c.Category).HasMaxLength(100);
-                }
     }
 }
