44d1bf0356f2c324bd06fdcf60d4c941e433aede
parks38 <parks38@uw.edu>
2019-07-14 13:06:42 -0700

    Update Program.cs

--- EventBriteAssignment3A/Program.cs
+++ EventBriteAssignment3A/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using EventBriteAssignment3A.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EventBriteCatalog
 {
