f112642117e1ab166d9acba73e48468b54521d7f
parks38 <parks38@uw.edu>
2019-07-14 13:00:13 -0700

    Update Startup.cs

--- EventBriteAssignment3A/Startup.cs
+++ EventBriteAssignment3A/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using EventBriteAssignment3A.Data;
 
 namespace EventBriteCatalog
 {
