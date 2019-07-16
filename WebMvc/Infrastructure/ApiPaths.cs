5a277f79691721699ab7db6f67b8a818594a18b3
Vivian Lau <lauv26@gmail.com>
2019-07-16 12:29:39 -0700

    added classes in infrastructure of WebMVC

---
+++ WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        public static class Catalog
+        {
+            // get categories, locations, and catalog items
+            public static string GetAllCategories(string baseUri)
+            {
+                // will return localhost/api/catalog/catalogcategories
+                return $"{baseUri}catalogcategories";
+            }
+             public static string GetAllLocations(string baseUri)
+            {
+                // will return localhost/api/catalog/cataloglocations
+                return $"{baseUri}cataloglocations";
+            }
+
+            // api path for catalog
+            public static string GetAllCatalogItems(string baseUri, int page, int take, int? category, int? location)
+            {
+                var filterQueries = string.Empty;
+
+                if(category.HasValue || location.HasValue)
+                {
+                    // if category has value, convert to a string, otherwise return null string
+                    var categoryQuery = (category.HasValue) ? category.Value.ToString() : "null";
+                    var locationQuery = (location.HasValue) ? location.Value.ToString() : "null";
+                    filterQueries = $"/category/{categoryQuery}/location/{locationQuery}";
+                }
+
+                return $"{baseUri}items{filterQueries}?pageSize={take}&pageIndex={page}";
+            }
+        }
+    }
+}
---
+++ WebMvc/Infrastructure/IHttpClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebMvc.Infrastructure
+{
+    public interface IHttpClient
+    {
+        // get call will return json format which contain strings
+        Task<string> GetStringAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
+
+        // method to create items
+        Task<HttpResponseMessage> PostAsync<Type>(string uri, Type item, string authorizationToken = null, string authorizationMethod = "Bearer");
+
+        // method to edit items
+        Task<HttpResponseMessage> PutAsync<Type>(string uri, Type item, string authorizationToken = null, string authorizationMethod = "Bearer");
+
+        Task<HttpResponseMessage> DeleteAsync(string uri, string authorizationToken = null, string authorizationMethod = "Bearer");
+    }
+}
