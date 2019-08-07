355982cb2900ec4474a91fa3db1f6352c39847e0
Vivian Lau <lauv26@gmail.com>
2019-08-06 19:12:00 -0700

    update namespace name

--- OrderApi/Models/OrderItem.cs
+++ OrderApi/Models/OrderItem.cs
@@ -1,4 +1,4 @@
-﻿using EventBriteAssignment3A.Services.OrderApi.Exceptions;
+﻿using EventBriteAssignment.Services.OrderApi.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
