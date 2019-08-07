356ac3cf07a3f41cdcad8b532a32dd0b0a5ec64d
Vivian Lau <lauv26@gmail.com>
2019-08-06 19:14:59 -0700

    update namespace name

--- OrderApi/Models/OrderItem.cs
+++ OrderApi/Models/OrderItem.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EventBriteAssignment3A.Services.OrderApi.Models
+namespace EventBriteAssignment.Services.OrderApi.Models
 {
     public class OrderItem
     {
