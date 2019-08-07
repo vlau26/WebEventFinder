cb39a143905ce03dbf02768b3505df5b17ae0418
mdoam <50501390+mdoam@users.noreply.github.com>
2019-08-07 13:16:24 -0700

    Update Cart.cs

--- CartApi/Model/Cart.cs
+++ CartApi/Model/Cart.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ShoesOnContainers.Services.CartApi.Model
+namespace EventBriteAssignment.Services.CartApi.Model
 {
     public class  Cart
     {
