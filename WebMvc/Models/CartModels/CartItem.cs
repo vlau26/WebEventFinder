2632cd5de5c77ac1d2f9b5045faa6367ed93ef8f
parks38 <parks38@uw.edu>
2019-08-08 10:59:37 -0700

    Update WebMvc/Models/CartModels/CartItem.cs
    Co-Authored-By: Vivian Lau <lauv26@gmail.com>

--- WebMvc/Models/CartModels/CartItem.cs
+++ WebMvc/Models/CartModels/CartItem.cs
@@ -9,7 +9,7 @@ namespace WebMvc.Models.CartModels
     {
         public string Id { get; set; }
         public string EventId { get; set; }
-        public string ProductName { get; set; }
+        public string EventName { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal OldUnitPrice { get; set; }
         public int Quantity { get; set; }
